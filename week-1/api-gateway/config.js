16a0038ae4f85901f4b14b0f1567e8cbfeafa8e0
Anil Rayamajhi <anilrayamajhi@Anils-iMac.local>
2020-10-27 17:38:46 -0700

    code attribution on index and config

--- week-1/api-gateway/config.js
+++ week-1/api-gateway/config.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  Assignment 2.3
+; Author: Professor Krasso
+; Date:   27 October 2020
+; Modified by: Anil Rayamajhi
+; Description: API Gateway Part II
+;===========================================
+*/
+
 var config = {};
 config.web = {};
 config.web.port = process.env.PORT || "3000";
--- week-1/api-gateway/routes/index.js
+++ week-1/api-gateway/routes/index.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  Assignment 2.3
+; Author: Professor Krasso
+; Date:   27 October 2020
+; Modified by: Anil Rayamajhi
+; Description: API Gateway Part II
+;===========================================
+*/
+
 var express = require("express");
 var router = express.Router();
 
