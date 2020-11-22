33dbc69aeeb1ebe680113146e1c233b8aa1addd9
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-11-22 13:56:12 -0800

    week 6 materials

---
+++ week-6/api-gateway-IV/models/user.js
@@ -0,0 +1,41 @@
+/*
+============================================
+; Title:  Assignment 6.3
+; Author: Professor Krasso
+; Date:   22 November 2020
+; Modified by: Anil Rayamajhi
+; Description: API Gateway Part IV
+;===========================================
+*/
+
+var mongoose = require("mongoose");
+
+/**
+    Fields username, password, and email
+*/
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+});
+
+const User = (module.exports = mongoose.model("User", userSchema));
+
+/**
+ Database queries
+ */
+
+// user.save to add a new user in database
+module.exports.add = (user, callback) => {
+  User.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+  var query = { _id: id };
+  User.findById(query, callback);
+};
+
+module.exports.getOne = (email, callback) => {
+  var query = { email: email };
+  User.findOne(query, callback);
+};
